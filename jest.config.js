ce595be9807bc6ba132dce739cf7f7f46906ea93
Sunny Hirai <thesunny@gmail.com>
2022-01-18 18:30:20 -0800

    Update jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  /**
-   * Configuration for getting `ts-jest` running
-   * https://github.com/kulshekhar/ts-jest
-   */
-  preset: "ts-jest",
-  testEnvironment: "node",
-}
---
+++ jest.once.config.js
@@ -0,0 +1,29 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  globals: {
+    "ts-jest": {
+      /**
+       * Change the tsconfig file used to `tsconfig.ts-jest.json`
+       */
+      tsconfig: "tsconfig.ts-jest.json",
+      /**
+       * Disable type checking which speeds up test running from about 3000ms
+       * on a tiny test to 122ms or a 250x speed up.
+       *
+       * We still get type checking in VSCode so it can be a good trade off.
+       *
+       * https://kulshekhar.github.io/ts-jest/docs/getting-started/options/
+       *
+       * Currently added in `jest.watch.config.js` which speeds up jest in
+       * watch mode significantly.
+       */
+      // isolatedModules: true,
+    },
+  },
+  /**
+   * Configuration for getting `ts-jest` running
+   * https://github.com/kulshekhar/ts-jest
+   */
+  preset: "ts-jest",
+  testEnvironment: "node",
+}
